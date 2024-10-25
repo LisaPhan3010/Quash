8c2b4a1d96f2f1332fc9d2ad9c4978705f4783d2
Lisa Phan <157837595+LisaPhan3010@users.noreply.github.com>
2024-10-24 20:19:21 -0500

    Create quash.h

---
+++ quash.h
@@ -0,0 +1,11 @@
+#ifndef QUASH_H
+#define QUASH_H
+
+#include "execute.h"
+#include "parse_interface.h"
+
+void initQuash(); //define quash
+void Quash(); //main function to run the shell
+void quashCleanup(); //function of cleaning before exiting the quash
+
+#endif
